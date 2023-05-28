7a2374c3a01dc4e9425fb6bccadb4f6db3d19560
Muhammed Can Küçükaslan <32893303+Kucukaslan@users.noreply.github.com>
2023-05-28 03:51:31 +0300

    implement gaussJacobi-serial-csc.c, update makefile

--- Makefile
+++ Makefile
@@ -1,18 +1,37 @@
-all: csr
+all: csc
 
 csr:
 	@gcc -o gaussJacobi-serial-csr src/util.h src/util.c  src/gaussJacobi-serial-csr.c -lm
-	./gaussJacobi-serial-csr ./input/3_matrix_A_csr.txt  ./input/3_matrix_b.txt outcsr3.txt
-	./gaussJacobi-serial-csr ./input/10_matrix_A_csr.txt  ./input/10_matrix_b.txt outcsr10.txt
-	./xdiff/xdiff ./outcsr3.txt  ./input/3_matrix_X.txt
-	./xdiff/xdiff ./outcsr10.txt  ./input/10_matrix_X.txt
+	@echo ""
+	@./gaussJacobi-serial-csr ./input/3_matrix_A_csr.txt  ./input/3_matrix_b.txt outcsr3.txt
+	@echo ""
+	@./gaussJacobi-serial-csr ./input/10_matrix_A_csr.txt  ./input/10_matrix_b.txt outcsr10.txt
+	@echo ""
+	@./gaussJacobi-serial-csr ./input/1000_matrix_A_csr.txt  ./input/1000_matrix_b.txt outcsr1000.txt
+	@echo ""
+	@./xdiff/xdiff ./outcsr3.txt  ./input/3_matrix_X.txt
+	@echo ""
+	@./xdiff/xdiff ./outcsr10.txt  ./input/10_matrix_X.txt
+	@echo ""
+	@./xdiff/xdiff ./outcsr1000.txt  ./input/1000_matrix_X.txt
 
 csc:
-	@gcc -o gaussJacobi-serial-csc src/util.h src/util.c  src/gaussJacobi-serial-csc.c 
-	./gaussJacobi-serial-csc ./input/3_matrix_A_csc.txt  ./input/3_matrix_b.txt outcsc.txt
+	@gcc -o gaussJacobi-serial-csc src/util.h src/util.c  src/gaussJacobi-serial-csc.c -lm
+	@echo ""
+	@./gaussJacobi-serial-csc ./input/3_matrix_A_csc.txt  ./input/3_matrix_b.txt outcsc3.txt
+	@echo ""
+	@./gaussJacobi-serial-csc ./input/10_matrix_A_csc.txt  ./input/10_matrix_b.txt outcsc10.txt
+	@echo ""
+	@./gaussJacobi-serial-csc ./input/1000_matrix_A_csc.txt  ./input/1000_matrix_b.txt outcsc1000.txt
+	@echo ""
+	@./xdiff/xdiff ./outcsc3.txt  ./input/3_matrix_X.txt
+	@echo ""
+	@./xdiff/xdiff ./outcsc10.txt  ./input/10_matrix_X.txt
+	@echo ""
+	@./xdiff/xdiff ./outcsc1000.txt  ./input/1000_matrix_X.txt
 
 mpi:
-	@mpicc -o sssp_parallel src/util.h src/util.c  src/sssp_parallel.c 
+	@mpicc -o sssp_parallel src/util.h src/util.c  src/sssp_parallel.c -lm
 #	mpirun -np 4 ./sssp_parallel input/sample_graph_paper_csc_8v_16e_weighted.txt 4 out.txt
 
 clear:
---
+++ src/gaussJacobi-serial-csc.c
@@ -0,0 +1,189 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <limits.h>
+#include "util.h"
+#include <stdbool.h>
+#include <math.h>
+
+// an example call is
+// %my_program input_matrix_A input_matrix_b output_matrix_X
+int main(int argc, char *argv[])
+{
+    // check if the number of arguments is correct
+    if (argc != 4)
+    {
+        printf("Usage: %s <input_file_A> <input_file_b> <output_file>\n", argv[0]);
+        return 1;
+    }
+
+    // offsets is the rows
+    // edges is the columns
+    // why did they named it like that? To confuse us?
+
+    // read the input file
+    int numVertices, numEdges, *offsets, *edges;
+    float *weights;
+    int success = read_matrix_csr(argv[1], &numVertices, &numEdges, &offsets, &edges, &weights);
+    if (!success)
+    {
+        printf("Error reading file %s\n", argv[1]);
+        return 1;
+    }
+    // else
+    // {
+    //     // print the matrix A
+    //     printf("With csr notation:\n");
+    //     for (int i = 0; i < numVertices; i++)
+    //     {
+    //         printf("%10d ", i);
+    //     }
+    //     printf("\n");
+
+    //     for (int i = 0; i < numVertices + 1; i++)
+    //     {
+    //         printf("%10d ", offsets[i]);
+    //     }
+    //     printf("\n");
+
+    //     for (int i = 0; i < numEdges; i++)
+    //     {
+    //         printf("%10d ", edges[i]);
+    //     }
+    //     printf("\n");
+
+    //     for (int i = 0; i < numEdges; i++)
+    //     {
+    //         printf("%10f ", weights[i]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // // print the matrix A'
+    // printf("A':\n");
+    // for (int i = 0; i < numVertices; i++)
+    // {
+    //     for (int j = 0; j < numVertices; j++)
+    //     {
+    //         bool found = false;
+    //         for (int k = offsets[i]; k < offsets[i + 1]; k++)
+    //         {
+    //             if (edges[k] == j)
+    //             {
+    //                 printf("%10f ", weights[k]);
+    //                 found = true;
+    //                 break;
+    //             }
+    //         }
+    //         if (!found)
+    //         {
+    //             printf("%10d ", 0);
+    //         }
+    //     }
+    //     printf("\n");
+    // }
+    // printf("\n");
+
+    // read the vector b from the input file
+    printf("Reading the vector b...\n");
+    int numVerticesB;
+    float *b;
+    success = read_vector(argv[2], &numVerticesB, &b);
+    if (!success)
+    {
+        printf("Error reading file %s\n", argv[2]);
+        return 1;
+    }
+    else
+    {
+        // // print the vector b
+        printf("read the b:\n");
+        // for (int i = 0; i < numVertices; i++)
+        // {
+        //     printf("%3f ", b[i]);
+        // }
+        // printf("\n");
+    }
+
+    // allocate memory for the vector x
+    float *x = (float *)malloc(numVertices * sizeof(float));
+    float *x_new = (float *)malloc(numVertices * sizeof(float));
+
+    // Xold = X // Initial solution vector (n x 1 vector filled with 0 values)
+    for (int i = 0; i < numVertices; i++)
+    {
+        x[i] = 0;
+    }
+
+    // apply the Gauss-Jacobi algorithm
+    printf("Applying the Gauss-Jacobi algorithm...\n");
+    float error = 100;
+    int iteration = 0;
+    while (error > EPSILON_e5)
+    {
+        for (int i = 0; i < numVertices; i++)
+        {
+            x_new[i] = 0;
+        }
+        // multiply A with x and store the result in x_new and add b
+        for (int i = 0; i < numVertices; i++)
+        {
+            for (int j = offsets[i]; j < offsets[i + 1]; j++)
+            {
+                // Transpose of this x_new[i] += weights[j] * x[edges[j]];
+                x_new[edges[j]] += weights[j] * x[i];
+            }
+        }
+
+        // add b
+        for (int i = 0; i < numVertices; i++)
+        {
+            x_new[i] += b[i];
+        }
+
+        error = 0;
+        for (int i = 0; i < numVertices; i++)
+        {
+            error += (x_new[i] - x[i]) * (x_new[i] - x[i]);
+        }
+        error = sqrtf(error);
+
+        // copy x_new to x
+        for (int i = 0; i < numVertices; i++)
+        {
+            x[i] = x_new[i];
+        }
+
+        if (iteration % 100 == 0)
+        {
+            printf("Iteration %d: error = %f\n", iteration, error);
+        }
+        // printf("Iteration %d: error = %f\n", iteration, error);
+        // printf("x:\n");
+        // for (int i = 0; i < numVertices; i++)
+        // {
+        //     printf("%3f ", x[i]);
+        // }
+        // printf("\n------------------\n");
+        iteration++;
+    }
+
+    // print the vector x
+    // printf("x:\n");
+    // for (int i = 0; i < numVertices; i++)
+    // {
+    //     printf("%3f ", x[i]);
+    // }
+    // printf("\n");
+
+    // write the vector x to the output file
+    printf("Writing the vector x to the output file %s...\n", argv[3]);
+    success = write_vector(argv[3], numVertices, x);
+    if (!success)
+    {
+        printf("Error writing file %s\n", argv[3]);
+        return 1;
+    }
+}
